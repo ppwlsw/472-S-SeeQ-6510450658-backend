b9b18b41f91ea7b26f6555cf88a62e3e2229ec76
Thanawat Potidet <thanawatpotidech@gmail.com>
2025-02-04 22:49:33 +0700

    can use pgsql

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function() {
+    return [
+        'success' => true,
+        'version' => '1.0.0',
+    ];
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
